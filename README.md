d9edcf2651cb39226316e56ceb056c2b6a6b3a70
xqhuang <1137512587@qq.com>
2023-08-13 21:41:26 +0800

    引用cmake工具

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # linux_learn
 # 学习Linux基础知识和实践项目
 # 项目工程开发
+# sample01
 1.解决代码耦合、逻辑不清晰和团队合作困难的问题，将功能代码模块化
 2.解决随着功能增多带来的项目责任田混淆，将创建模块功能文件夹
 3.解决编译困难、繁琐问题，引入make工具
-4.解决编译耦合，无法跨平台构建问题，引入cmake工具
-
 ## build
     //gcc编译
     gcc -o main main.c hello/hello.c world/world.c -I world
@@ -26,3 +25,12 @@
     clean:
 	    rm -rf main main.o hello.o world.o
     //引入cmake，创建CMakeLists.txt
+
+# sample02
+1.无法跨平台构建问题，引入cmake工具,创建CMakeLists.txt
+    //创建存放过程文件的文件夹
+    mkdir build
+    cmake ../
+    make
+    //显示详细过程
+    make VERBOSE=1
---
+++ sample02/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/xqhuang/linux_learn/sample02/hello/hello.c" "CMakeFiles/main.dir/hello/hello.c.o" "gcc" "CMakeFiles/main.dir/hello/hello.c.o.d"
+  "/root/xqhuang/linux_learn/sample02/main.c" "CMakeFiles/main.dir/main.c.o" "gcc" "CMakeFiles/main.dir/main.c.o.d"
+  "/root/xqhuang/linux_learn/sample02/world/world.c" "CMakeFiles/main.dir/world/world.c.o" "gcc" "CMakeFiles/main.dir/world/world.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
